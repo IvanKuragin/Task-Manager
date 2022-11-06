a7cfcb0f7df05cbc823b02fa74986f12f0aae5fd
Ivan <vanya.kuragin@yandex.ru>
2022-11-06 08:03:50 +0300

    Changed List into Set, corrected magical number, corrected String output

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,13 +1,14 @@
 package ru.netology.javacore;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Todos {
-    private List<String> tasks = new ArrayList<>();
+
+    private final int LIST_SIZE = 7;
+    private Set<String> tasks = new TreeSet<>();
 
     public void addTask(String task) {
-        if (tasks.size() < 7) {
+        if (tasks.size() < LIST_SIZE) {
             tasks.add(task);
         } else {
             tasks.add(task);
@@ -20,9 +21,9 @@ public class Todos {
     }
 
     public String getAllTasks() {
-        List<String> outTasks = tasks.stream()
-                .sorted()
-                .collect(Collectors.toList());
-        return Arrays.toString(outTasks.toArray());
+        return Arrays.toString(tasks.toArray())
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "");
     }
 }
